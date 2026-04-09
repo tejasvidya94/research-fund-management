3101252c7b37bc7fa2f501c6030dab6e3045f857
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-04-09 23:54:56 +0530

    created constants folder and created file workflow.js for constants like designation to stage and stages that have been used in equipment.controller.js and project.controller.js

---
+++ backend/src/constants/workflow.js
@@ -0,0 +1,26 @@
+exports.DESIGNATION_TO_STAGE = {
+    hod: 'HOD',
+    dean: 'DEAN',
+    rnd_helper: 'R&D_HELPER',
+    rnd_main: 'R&D_MAIN',
+    aio: 'ACADEMIC_INTEGRITY_OFFICER',
+    finance_officer_helper: 'FINANCE_OFFICER_HELPER',
+    finance_officer_main: 'FINANCE_OFFICER_MAIN',
+    registrar: 'REGISTRAR',
+    vc_office: 'VC_OFFICE',
+    vice_chancellor: 'VICE_CHANCELLOR'
+};
+
+exports.STAGES = [
+    'HOD',
+    'DEAN',
+    'R&D_HELPER',
+    'R&D_MAIN',
+    'ACADEMIC_INTEGRITY_OFFICER',
+    'FINANCE_OFFICER_HELPER',
+    'FINANCE_OFFICER_MAIN',
+    'REGISTRAR',
+    'VC_OFFICE',
+    'VICE_CHANCELLOR',
+    'COMPLETED'
+];
